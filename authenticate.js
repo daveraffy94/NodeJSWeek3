6007d5938ae0220a367ca3b1349f176d66ae51eb
Dave R <75257240+daveraffy94@users.noreply.github.com>
2021-07-13 16:45:19 -0400

    Add files via upload

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,50 @@
 const passport = require("passport");
 const User = require("./models/user");
 const LocalStrategy = require("passport-local").Strategy;
+
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+const config = require('./config.js')
+const jwt = require('jsonwebtoken'); 
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+exports.getToken = function(user) {
+    return jwt.sign(user, config.secretKey, {expiresIn: 3600}); 
+};
+​
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); 
+opts.secretOrKey = config.secretKey;
+​
+exports.jwtPassport = passport.use(
+    new JwtStrategy(
+        opts,
+        (jwt_payload, done) => {
+            console.log('JWT payload:', jwt_payload);
+            User.findOne({_id: jwt_payload._id}, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                } else if (user) {
+                    return done(null, user);
+                } else {
+                    return done(null, false);
+                }
+            });
+        }
+    )
+);
+​
+exports.verifyUser = passport.authenticate('jwt', {session: false});    
+​
+exports.verifyAdmin = function(req, res, next){
+    if (req.user.admin === true){
+        return next();
+    } else {
+        const err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
+    }
+};
---
+++ favoriteRouter.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const favoriteRouter = express.Router();
+const Favorite = require("../models/favorite");
+const authenticate = require("../authenticate");
+const cors = require("./cors");
+favoriteRouter
+  .route("/")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
+    Favorite.find({ user: req.user._id })
+      .populate("user")
+      .populate("campsites")
+      .then((favorites) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(favorites);
+      })
+      .catch((err) => next(err));
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOne({ user: req.user._id })
+      .then((favorite) => {
+        if (favorite) {
+          req.body.forEach((fav) => {
+            if (!favorite.campsites.includes(fav._id)) {
+              favorite.campsites.push(fav._id);
+            }
+          });
+          favorite
+            .save()
+            .then((favorite) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(favorite);
+            })
+            .catch((err) => next(err));
+        } else {
+          Favorite.create({ user: req.user._id })
+            .then((favorite) => {
+              req.body.forEach((fav) => {
+                if (!favorite.campsites.includes(fav._id)) {
+                  favorite.campsites.push(fav._id);
+                }
+              });
+              favorite
+                .save()
+                .then((favorite) => {
+                  res.statusCode = 200;
+                  res.setHeader("Content-Type", "application/json");
+                  res.json(favorite);
+                })
+                .catch((err) => next(err));
+            })
+            .catch((err) => next(err));
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /favorites");
+  })
+  .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOneAndDelete({ user: req.user._id })
+      .then((favorite) => {
+        if (favorite) {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(favorite);
+        } else {
+          res.setHeader("Content-Type", "text/plain");
+          res.end("You do not have any favorites to delete");
+        }
+      })
+      .catch((err) => next(err));
+  });
+favoriteRouter
+  .route("/:campsiteId")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /favorites");
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOne(req.params._id).then((favorites) => {
+      if (!favorites.includes(req.params)) {
+        favorites.campsites.push(req.body);
+      } else {
+        res.end("That campsite is already in the list of favorites!");
+      }
+    });
+  })
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /favorites");
+  })
+  .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findOne({ user: req.user._id })
+      .then((favorite) => {
+        if (favorite) {
+          favorite.campsites = favorite.campsites.filter(
+            (fave) => fave.toString() !== req.params.campsiteId.toString()
+          );
+          favorite
+            .save()
+            .then((favorite) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(favorite);
+            })
+            .catch((err) => next(err));
+        } else {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          res.end("you do not have any favorites to delete");
+        }
+      })
+      .catch((err) => next(err));
+  });
+module.exports = favoriteRouter;
